26fdd4ade62c8b59cea9db033951ca75c17013a4
devpaal1995 <devendrapal181@gmail.com>
2022-07-31 13:38:22 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Sdata from "./Sdata"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <> 
+  <h1 className="heading_style"> List of top Netflix series 2022</h1>
+  
+  {Sdata.map((val) => {
+    return (
+    <Card  
+    imgsrc={val.imgsrc}
+     title={val.title}
+      sname={val.sname}
+       link={val.link}
+       />
+    )
+  })}
+  
+ 
+  </>,
+  document.getElementById("root")
+)
